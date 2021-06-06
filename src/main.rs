f363b00c6f5032a39284e4e62ec4b7b6fa957c50
Enos Domingues <enosdomingues@gmail.com>
2021-06-06 16:59:54 -0300

    trying to authenticate

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::time::Instant;
-
-use hyper::{Client, Uri, body::Buf};
+use std::time::{SystemTime, UNIX_EPOCH};
+use hyper::{Client, /* Body, Method, Request, */ Uri, body::Buf};
 use hyper_tls::HttpsConnector;
 use serde::{Deserialize, Serialize};
+use sha2::Sha256;
+use hmac::{Hmac, Mac, NewMac};
+extern crate hex;
+
+type HmacSha256 = Hmac<Sha256>;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct SingleMarketResult {
@@ -19,6 +24,44 @@ struct SingleMarket {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let start = Instant::now();
+
+    /* let key = "7LBTqPT3g4vhpZ1mNoQN8nQPm-xmqPHvknnPUUR3"; */
+
+    let secret = "xrTiHQj3XSul0-7LL-MUD9mqb3WOTiYws24bb0fh";
+
+    let ts: String = 
+        SystemTime::now().duration_since(UNIX_EPOCH)
+        .expect("Time went backwards").as_millis().to_string();
+
+    let signature_payload = format!("{}{}", ts, "POST/api/orders
+    {
+        \"market\": \"BTC/USD\", 
+        \"side\": \"buy\", 
+        \"type\": \"market\", 
+        \"size\": \"0.0001\", 
+        \"externalReferralProgram\": \"oBot\", 
+    }");
+
+    let signature: String = format!("{}{}", secret, signature_payload);
+
+    let mac = HmacSha256::new_from_slice(signature.as_bytes()).unwrap();
+
+    let result = mac.finalize();
+
+    let code_bytes = result.into_bytes();
+
+    let encoded = hex::encode(&code_bytes);
+
+    println!("{:#?}", encoded);
+
+    /* let req = Request::builder()
+    .method(Method::POST)
+    .uri("https://ftx.com/api")
+    .header("content-type", "application/json")
+    .header("FTX-KEY", key)
+    .header("FTX-TS", ts)
+    .header("FTX-SIGN", signature)
+    .body(Body::from(r#"{"library":"hyper"}"#))?; */
     
     let https = HttpsConnector::new();
 
